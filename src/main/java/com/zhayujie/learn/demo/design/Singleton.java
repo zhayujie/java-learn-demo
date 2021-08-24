f9254d03b349a46ae46dce53b942389ef8010a6b
yujiezha <yujiezha@tencent.com>
2021-08-24 10:35:23 +0800

    add singleton

--- src/main/java/com/zhayujie/learn/demo/design/Singleton.java
+++ src/main/java/com/zhayujie/learn/demo/design/Singleton.java
@@ -6,4 +6,30 @@ package com.zhayujie.learn.demo.design;
  * @date 2021/8/23
  */
 public class Singleton {
+    /**
+     * 1.static + volatile
+     */
+    private static volatile Singleton instance;
+
+    /**
+     * 2.private 构造
+     */
+    private Singleton() {
+    }
+
+    public static Singleton newInstance() {
+        if (instance == null) {
+            // 3.双检索
+            synchronized (Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Singleton.newInstance());
+    }
 }
